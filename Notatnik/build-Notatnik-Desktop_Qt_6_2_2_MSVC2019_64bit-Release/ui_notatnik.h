3b729a7402c491d3e573f9fd376eb419def6b63c
Lukmajcher <lmajcher13@gmail.com>
2022-01-26 19:34:41 +0100

    v 0.0.4

---
+++ Notatnik/build-Notatnik-Desktop_Qt_6_2_2_MSVC2019_64bit-Release/ui_notatnik.h
@@ -0,0 +1,314 @@
+/********************************************************************************
+** Form generated from reading UI file 'notatnik.ui'
+**
+** Created by: Qt User Interface Compiler version 6.2.2
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_NOTATNIK_H
+#define UI_NOTATNIK_H
+
+#include <QtCore/QLocale>
+#include <QtCore/QVariant>
+#include <QtGui/QAction>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenu>
+#include <QtWidgets/QMenuBar>
+#include <QtWidgets/QStatusBar>
+#include <QtWidgets/QTextEdit>
+#include <QtWidgets/QToolBar>
+#include <QtWidgets/QVBoxLayout>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_MainWindow
+{
+public:
+    QAction *actionNowy;
+    QAction *actionNoweOkno;
+    QAction *actionOtworz;
+    QAction *actionZapisz;
+    QAction *actionZapiszJako;
+    QAction *actionDrukuj;
+    QAction *actionWyjdz;
+    QAction *actionCofnij;
+    QAction *actionWytnij;
+    QAction *actionKopiuj;
+    QAction *actionWklej;
+    QAction *actionUsun;
+    QAction *actionWyszukajWInternecie;
+    QAction *actionZnajdz;
+    QAction *actionIdzDo;
+    QAction *actionZaznaczWszystko;
+    QAction *actionDzisiejszaData;
+    QAction *actionPowtorz;
+    QAction *actionZawijanie_wierszy;
+    QAction *actionCzcionka;
+    QAction *actionO_programie_2;
+    QAction *actionKontakt;
+    QWidget *centralwidget;
+    QWidget *verticalLayoutWidget;
+    QVBoxLayout *verticalLayout;
+    QTextEdit *textEdit;
+    QMenuBar *menubar;
+    QMenu *menuPlik;
+    QMenu *menuEdycja;
+    QMenu *menu_Format;
+    QMenu *menuPomoc;
+    QStatusBar *statusbar;
+    QToolBar *toolBar;
+
+    void setupUi(QMainWindow *MainWindow)
+    {
+        if (MainWindow->objectName().isEmpty())
+            MainWindow->setObjectName(QString::fromUtf8("MainWindow"));
+        MainWindow->resize(800, 600);
+        MainWindow->setWindowOpacity(1.000000000000000);
+        MainWindow->setUnifiedTitleAndToolBarOnMac(true);
+        actionNowy = new QAction(MainWindow);
+        actionNowy->setObjectName(QString::fromUtf8("actionNowy"));
+        QIcon icon;
+        icon.addFile(QString::fromUtf8(":/imgs/img/New.ico"), QSize(), QIcon::Normal, QIcon::Off);
+        actionNowy->setIcon(icon);
+        actionNoweOkno = new QAction(MainWindow);
+        actionNoweOkno->setObjectName(QString::fromUtf8("actionNoweOkno"));
+        QIcon icon1;
+        icon1.addFile(QString::fromUtf8(":/imgs/img/Open.ico"), QSize(), QIcon::Normal, QIcon::On);
+        actionNoweOkno->setIcon(icon1);
+        actionOtworz = new QAction(MainWindow);
+        actionOtworz->setObjectName(QString::fromUtf8("actionOtworz"));
+        actionOtworz->setIcon(icon1);
+        actionZapisz = new QAction(MainWindow);
+        actionZapisz->setObjectName(QString::fromUtf8("actionZapisz"));
+        QIcon icon2;
+        icon2.addFile(QString::fromUtf8(":/imgs/img/Save.ico"), QSize(), QIcon::Normal, QIcon::On);
+        actionZapisz->setIcon(icon2);
+        actionZapiszJako = new QAction(MainWindow);
+        actionZapiszJako->setObjectName(QString::fromUtf8("actionZapiszJako"));
+        QIcon icon3;
+        icon3.addFile(QString::fromUtf8(":/imgs/img/SaveAs.ico"), QSize(), QIcon::Normal, QIcon::On);
+        actionZapiszJako->setIcon(icon3);
+        actionDrukuj = new QAction(MainWindow);
+        actionDrukuj->setObjectName(QString::fromUtf8("actionDrukuj"));
+        QIcon icon4;
+        icon4.addFile(QString::fromUtf8(":/imgs/img/Print.ico"), QSize(), QIcon::Normal, QIcon::On);
+        actionDrukuj->setIcon(icon4);
+        actionWyjdz = new QAction(MainWindow);
+        actionWyjdz->setObjectName(QString::fromUtf8("actionWyjdz"));
+        QIcon icon5;
+        icon5.addFile(QString::fromUtf8(":/imgs/img/Exit.ico"), QSize(), QIcon::Normal, QIcon::On);
+        actionWyjdz->setIcon(icon5);
+        actionCofnij = new QAction(MainWindow);
+        actionCofnij->setObjectName(QString::fromUtf8("actionCofnij"));
+        QIcon icon6;
+        icon6.addFile(QString::fromUtf8(":/imgs/img/undo.png"), QSize(), QIcon::Normal, QIcon::Off);
+        actionCofnij->setIcon(icon6);
+        actionWytnij = new QAction(MainWindow);
+        actionWytnij->setObjectName(QString::fromUtf8("actionWytnij"));
+        QIcon icon7;
+        icon7.addFile(QString::fromUtf8(":/imgs/img/Cut.ico"), QSize(), QIcon::Normal, QIcon::On);
+        actionWytnij->setIcon(icon7);
+        actionKopiuj = new QAction(MainWindow);
+        actionKopiuj->setObjectName(QString::fromUtf8("actionKopiuj"));
+        QIcon icon8;
+        icon8.addFile(QString::fromUtf8(":/imgs/img/Copy.ico"), QSize(), QIcon::Normal, QIcon::Off);
+        actionKopiuj->setIcon(icon8);
+        actionWklej = new QAction(MainWindow);
+        actionWklej->setObjectName(QString::fromUtf8("actionWklej"));
+        QIcon icon9;
+        icon9.addFile(QString::fromUtf8(":/imgs/img/Paste.ico"), QSize(), QIcon::Normal, QIcon::On);
+        actionWklej->setIcon(icon9);
+        actionUsun = new QAction(MainWindow);
+        actionUsun->setObjectName(QString::fromUtf8("actionUsun"));
+        actionUsun->setIcon(icon5);
+        actionWyszukajWInternecie = new QAction(MainWindow);
+        actionWyszukajWInternecie->setObjectName(QString::fromUtf8("actionWyszukajWInternecie"));
+        actionZnajdz = new QAction(MainWindow);
+        actionZnajdz->setObjectName(QString::fromUtf8("actionZnajdz"));
+        QIcon icon10;
+        icon10.addFile(QString::fromUtf8(":/imgs/img/Find.ico"), QSize(), QIcon::Normal, QIcon::On);
+        actionZnajdz->setIcon(icon10);
+        actionIdzDo = new QAction(MainWindow);
+        actionIdzDo->setObjectName(QString::fromUtf8("actionIdzDo"));
+        actionZaznaczWszystko = new QAction(MainWindow);
+        actionZaznaczWszystko->setObjectName(QString::fromUtf8("actionZaznaczWszystko"));
+        actionDzisiejszaData = new QAction(MainWindow);
+        actionDzisiejszaData->setObjectName(QString::fromUtf8("actionDzisiejszaData"));
+        actionPowtorz = new QAction(MainWindow);
+        actionPowtorz->setObjectName(QString::fromUtf8("actionPowtorz"));
+        QIcon icon11;
+        icon11.addFile(QString::fromUtf8(":/imgs/img/redo.png"), QSize(), QIcon::Normal, QIcon::On);
+        actionPowtorz->setIcon(icon11);
+        actionZawijanie_wierszy = new QAction(MainWindow);
+        actionZawijanie_wierszy->setObjectName(QString::fromUtf8("actionZawijanie_wierszy"));
+        actionZawijanie_wierszy->setCheckable(true);
+        actionZawijanie_wierszy->setChecked(true);
+        actionCzcionka = new QAction(MainWindow);
+        actionCzcionka->setObjectName(QString::fromUtf8("actionCzcionka"));
+        actionO_programie_2 = new QAction(MainWindow);
+        actionO_programie_2->setObjectName(QString::fromUtf8("actionO_programie_2"));
+        QIcon icon12;
+        icon12.addFile(QString::fromUtf8(":/imgs/img/info.png"), QSize(), QIcon::Normal, QIcon::On);
+        actionO_programie_2->setIcon(icon12);
+        actionKontakt = new QAction(MainWindow);
+        actionKontakt->setObjectName(QString::fromUtf8("actionKontakt"));
+        centralwidget = new QWidget(MainWindow);
+        centralwidget->setObjectName(QString::fromUtf8("centralwidget"));
+        verticalLayoutWidget = new QWidget(centralwidget);
+        verticalLayoutWidget->setObjectName(QString::fromUtf8("verticalLayoutWidget"));
+        verticalLayoutWidget->setGeometry(QRect(100, 90, 641, 401));
+        verticalLayout = new QVBoxLayout(verticalLayoutWidget);
+        verticalLayout->setObjectName(QString::fromUtf8("verticalLayout"));
+        verticalLayout->setContentsMargins(0, 0, 0, 0);
+        textEdit = new QTextEdit(verticalLayoutWidget);
+        textEdit->setObjectName(QString::fromUtf8("textEdit"));
+        QFont font;
+        font.setPointSize(12);
+        textEdit->setFont(font);
+        textEdit->setLocale(QLocale(QLocale::Polish, QLocale::Poland));
+
+        verticalLayout->addWidget(textEdit);
+
+        MainWindow->setCentralWidget(centralwidget);
+        menubar = new QMenuBar(MainWindow);
+        menubar->setObjectName(QString::fromUtf8("menubar"));
+        menubar->setGeometry(QRect(0, 0, 800, 21));
+        menuPlik = new QMenu(menubar);
+        menuPlik->setObjectName(QString::fromUtf8("menuPlik"));
+        menuEdycja = new QMenu(menubar);
+        menuEdycja->setObjectName(QString::fromUtf8("menuEdycja"));
+        menu_Format = new QMenu(menubar);
+        menu_Format->setObjectName(QString::fromUtf8("menu_Format"));
+        menuPomoc = new QMenu(menubar);
+        menuPomoc->setObjectName(QString::fromUtf8("menuPomoc"));
+        MainWindow->setMenuBar(menubar);
+        statusbar = new QStatusBar(MainWindow);
+        statusbar->setObjectName(QString::fromUtf8("statusbar"));
+        MainWindow->setStatusBar(statusbar);
+        toolBar = new QToolBar(MainWindow);
+        toolBar->setObjectName(QString::fromUtf8("toolBar"));
+        MainWindow->addToolBar(Qt::TopToolBarArea, toolBar);
+
+        menubar->addAction(menuPlik->menuAction());
+        menubar->addAction(menuEdycja->menuAction());
+        menubar->addAction(menu_Format->menuAction());
+        menubar->addAction(menuPomoc->menuAction());
+        menuPlik->addAction(actionNowy);
+        menuPlik->addAction(actionOtworz);
+        menuPlik->addAction(actionZapisz);
+        menuPlik->addAction(actionZapiszJako);
+        menuPlik->addSeparator();
+        menuPlik->addAction(actionDrukuj);
+        menuPlik->addSeparator();
+        menuPlik->addAction(actionWyjdz);
+        menuEdycja->addAction(actionCofnij);
+        menuEdycja->addAction(actionPowtorz);
+        menuEdycja->addSeparator();
+        menuEdycja->addAction(actionWytnij);
+        menuEdycja->addAction(actionKopiuj);
+        menuEdycja->addAction(actionWklej);
+        menuEdycja->addAction(actionUsun);
+        menuEdycja->addSeparator();
+        menuEdycja->addAction(actionWyszukajWInternecie);
+        menuEdycja->addAction(actionDzisiejszaData);
+        menuEdycja->addSeparator();
+        menuEdycja->addSeparator();
+        menu_Format->addAction(actionZawijanie_wierszy);
+        menu_Format->addAction(actionCzcionka);
+        menuPomoc->addAction(actionO_programie_2);
+        menuPomoc->addAction(actionKontakt);
+        toolBar->addAction(actionNowy);
+        toolBar->addAction(actionOtworz);
+        toolBar->addAction(actionZapisz);
+        toolBar->addAction(actionZapiszJako);
+        toolBar->addSeparator();
+        toolBar->addAction(actionCofnij);
+        toolBar->addAction(actionPowtorz);
+        toolBar->addSeparator();
+        toolBar->addAction(actionKopiuj);
+        toolBar->addAction(actionWklej);
+        toolBar->addAction(actionWytnij);
+        toolBar->addSeparator();
+        toolBar->addAction(actionDrukuj);
+        toolBar->addAction(actionO_programie_2);
+        toolBar->addSeparator();
+
+        retranslateUi(MainWindow);
+
+        QMetaObject::connectSlotsByName(MainWindow);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *MainWindow)
+    {
+        MainWindow->setWindowTitle(QCoreApplication::translate("MainWindow", "Notatnik", nullptr));
+        actionNowy->setText(QCoreApplication::translate("MainWindow", "Nowy", nullptr));
+#if QT_CONFIG(tooltip)
+        actionNowy->setToolTip(QCoreApplication::translate("MainWindow", "Nowy", nullptr));
+#endif // QT_CONFIG(tooltip)
+#if QT_CONFIG(shortcut)
+        actionNowy->setShortcut(QCoreApplication::translate("MainWindow", "Ctrl+N", nullptr));
+#endif // QT_CONFIG(shortcut)
+        actionNoweOkno->setText(QCoreApplication::translate("MainWindow", "Nowe okno", nullptr));
+        actionOtworz->setText(QCoreApplication::translate("MainWindow", "Otw\303\263rz", nullptr));
+#if QT_CONFIG(tooltip)
+        actionOtworz->setToolTip(QCoreApplication::translate("MainWindow", "Otw\303\263rz", nullptr));
+#endif // QT_CONFIG(tooltip)
+#if QT_CONFIG(shortcut)
+        actionOtworz->setShortcut(QCoreApplication::translate("MainWindow", "Ctrl+O", nullptr));
+#endif // QT_CONFIG(shortcut)
+        actionZapisz->setText(QCoreApplication::translate("MainWindow", "Zapisz", nullptr));
+#if QT_CONFIG(shortcut)
+        actionZapisz->setShortcut(QCoreApplication::translate("MainWindow", "Ctrl+S", nullptr));
+#endif // QT_CONFIG(shortcut)
+        actionZapiszJako->setText(QCoreApplication::translate("MainWindow", "Zapisz jako...", nullptr));
+        actionDrukuj->setText(QCoreApplication::translate("MainWindow", "Drukuj", nullptr));
+        actionWyjdz->setText(QCoreApplication::translate("MainWindow", "Wyjd\305\272", nullptr));
+        actionCofnij->setText(QCoreApplication::translate("MainWindow", "Cofnij", nullptr));
+#if QT_CONFIG(shortcut)
+        actionCofnij->setShortcut(QCoreApplication::translate("MainWindow", "Ctrl+Z", nullptr));
+#endif // QT_CONFIG(shortcut)
+        actionWytnij->setText(QCoreApplication::translate("MainWindow", "Wytnij", nullptr));
+        actionKopiuj->setText(QCoreApplication::translate("MainWindow", "Kopiuj", nullptr));
+        actionWklej->setText(QCoreApplication::translate("MainWindow", "Wklej", nullptr));
+        actionUsun->setText(QCoreApplication::translate("MainWindow", "Usu\305\204", nullptr));
+#if QT_CONFIG(shortcut)
+        actionUsun->setShortcut(QCoreApplication::translate("MainWindow", "Del", nullptr));
+#endif // QT_CONFIG(shortcut)
+        actionWyszukajWInternecie->setText(QCoreApplication::translate("MainWindow", "Wyszukaj w Internecie", nullptr));
+        actionZnajdz->setText(QCoreApplication::translate("MainWindow", "Znajd\305\272", nullptr));
+#if QT_CONFIG(shortcut)
+        actionZnajdz->setShortcut(QCoreApplication::translate("MainWindow", "Ctrl+F", nullptr));
+#endif // QT_CONFIG(shortcut)
+        actionIdzDo->setText(QCoreApplication::translate("MainWindow", "Id\305\272 do", nullptr));
+        actionZaznaczWszystko->setText(QCoreApplication::translate("MainWindow", "Zaznacz wszystko", nullptr));
+#if QT_CONFIG(shortcut)
+        actionZaznaczWszystko->setShortcut(QCoreApplication::translate("MainWindow", "Ctrl+A", nullptr));
+#endif // QT_CONFIG(shortcut)
+        actionDzisiejszaData->setText(QCoreApplication::translate("MainWindow", "Dzisiejsza data", nullptr));
+        actionPowtorz->setText(QCoreApplication::translate("MainWindow", "Powt\303\263rz", nullptr));
+#if QT_CONFIG(shortcut)
+        actionPowtorz->setShortcut(QCoreApplication::translate("MainWindow", "Ctrl+Y", nullptr));
+#endif // QT_CONFIG(shortcut)
+        actionZawijanie_wierszy->setText(QCoreApplication::translate("MainWindow", "Zawijanie wierszy", nullptr));
+        actionCzcionka->setText(QCoreApplication::translate("MainWindow", "Czcionka", nullptr));
+        actionO_programie_2->setText(QCoreApplication::translate("MainWindow", "O programie", nullptr));
+        actionKontakt->setText(QCoreApplication::translate("MainWindow", "Kontakt", nullptr));
+        menuPlik->setTitle(QCoreApplication::translate("MainWindow", "&Plik", nullptr));
+        menuEdycja->setTitle(QCoreApplication::translate("MainWindow", "&Edycja", nullptr));
+        menu_Format->setTitle(QCoreApplication::translate("MainWindow", "&Format", nullptr));
+        menuPomoc->setTitle(QCoreApplication::translate("MainWindow", "P&omoc", nullptr));
+        toolBar->setWindowTitle(QCoreApplication::translate("MainWindow", "toolBar", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class MainWindow: public Ui_MainWindow {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_NOTATNIK_H
--- Notatnik/build-Notatnik-Desktop_Qt_6_2_2_MinGW_64_bit-Debug/debug/moc_notatnik.cpp
+++ Notatnik/build-Notatnik-Desktop_Qt_6_2_2_MinGW_64_bit-Debug/debug/moc_notatnik.cpp
@@ -23,7 +23,7 @@ QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
 struct qt_meta_stringdata_MainWindow_t {
     const uint offsetsAndSize[38];
-    char stringdata0[445];
+    char stringdata0[497];
 };
 #define QT_MOC_LITERAL(ofs, len) \
     uint(offsetof(qt_meta_stringdata_MainWindow_t, stringdata0) + ofs), len 
@@ -36,26 +36,26 @@ QT_MOC_LITERAL(36, 25), // "on_actionOtworz_triggered"
 QT_MOC_LITERAL(62, 29), // "on_actionZapiszJako_triggered"
 QT_MOC_LITERAL(92, 25), // "on_actionDrukuj_triggered"
 QT_MOC_LITERAL(118, 24), // "on_actionWyjdz_triggered"
-QT_MOC_LITERAL(143, 12), // "QCloseEvent*"
-QT_MOC_LITERAL(156, 5), // "event"
-QT_MOC_LITERAL(162, 25), // "on_actionKopiuj_triggered"
-QT_MOC_LITERAL(188, 24), // "on_actionWklej_triggered"
-QT_MOC_LITERAL(213, 25), // "on_actionWytnij_triggered"
-QT_MOC_LITERAL(239, 25), // "on_actionCofnij_triggered"
-QT_MOC_LITERAL(265, 26), // "on_actionPowtorz_triggered"
-QT_MOC_LITERAL(292, 25), // "on_actionZapisz_triggered"
-QT_MOC_LITERAL(318, 32), // "on_actionO_programie_2_triggered"
-QT_MOC_LITERAL(351, 27), // "on_actionCzcionka_triggered"
-QT_MOC_LITERAL(379, 26), // "on_actionKontakt_triggered"
-QT_MOC_LITERAL(406, 38) // "on_actionWyszukajWInternecie_..."
+QT_MOC_LITERAL(143, 25), // "on_actionKopiuj_triggered"
+QT_MOC_LITERAL(169, 24), // "on_actionWklej_triggered"
+QT_MOC_LITERAL(194, 25), // "on_actionWytnij_triggered"
+QT_MOC_LITERAL(220, 25), // "on_actionCofnij_triggered"
+QT_MOC_LITERAL(246, 26), // "on_actionPowtorz_triggered"
+QT_MOC_LITERAL(273, 25), // "on_actionZapisz_triggered"
+QT_MOC_LITERAL(299, 32), // "on_actionO_programie_2_triggered"
+QT_MOC_LITERAL(332, 27), // "on_actionCzcionka_triggered"
+QT_MOC_LITERAL(360, 26), // "on_actionKontakt_triggered"
+QT_MOC_LITERAL(387, 38), // "on_actionWyszukajWInternecie_..."
+QT_MOC_LITERAL(426, 36), // "on_actionZawijanie_wierszy_tr..."
+QT_MOC_LITERAL(463, 33) // "on_actionDzisiejszaData_trigg..."
 
     },
     "MainWindow\0on_actionNowy_triggered\0\0"
     "on_actionOtworz_triggered\0"
     "on_actionZapiszJako_triggered\0"
     "on_actionDrukuj_triggered\0"
-    "on_actionWyjdz_triggered\0QCloseEvent*\0"
-    "event\0on_actionKopiuj_triggered\0"
+    "on_actionWyjdz_triggered\0"
+    "on_actionKopiuj_triggered\0"
     "on_actionWklej_triggered\0"
     "on_actionWytnij_triggered\0"
     "on_actionCofnij_triggered\0"
@@ -64,7 +64,9 @@ QT_MOC_LITERAL(406, 38) // "on_actionWyszukajWInternecie_..."
     "on_actionO_programie_2_triggered\0"
     "on_actionCzcionka_triggered\0"
     "on_actionKontakt_triggered\0"
-    "on_actionWyszukajWInternecie_triggered"
+    "on_actionWyszukajWInternecie_triggered\0"
+    "on_actionZawijanie_wierszy_triggered\0"
+    "on_actionDzisiejszaData_triggered"
 };
 #undef QT_MOC_LITERAL
 
@@ -74,7 +76,7 @@ static const uint qt_meta_data_MainWindow[] = {
       10,       // revision
        0,       // classname
        0,    0, // classinfo
-      15,   14, // methods
+      17,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -82,28 +84,32 @@ static const uint qt_meta_data_MainWindow[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags, initial metatype offsets
-       1,    0,  104,    2, 0x08,    1 /* Private */,
-       3,    0,  105,    2, 0x08,    2 /* Private */,
-       4,    0,  106,    2, 0x08,    3 /* Private */,
-       5,    0,  107,    2, 0x08,    4 /* Private */,
-       6,    1,  108,    2, 0x08,    5 /* Private */,
-       9,    0,  111,    2, 0x08,    7 /* Private */,
-      10,    0,  112,    2, 0x08,    8 /* Private */,
-      11,    0,  113,    2, 0x08,    9 /* Private */,
-      12,    0,  114,    2, 0x08,   10 /* Private */,
-      13,    0,  115,    2, 0x08,   11 /* Private */,
-      14,    0,  116,    2, 0x08,   12 /* Private */,
-      15,    0,  117,    2, 0x08,   13 /* Private */,
-      16,    0,  118,    2, 0x08,   14 /* Private */,
-      17,    0,  119,    2, 0x08,   15 /* Private */,
-      18,    0,  120,    2, 0x08,   16 /* Private */,
+       1,    0,  116,    2, 0x08,    1 /* Private */,
+       3,    0,  117,    2, 0x08,    2 /* Private */,
+       4,    0,  118,    2, 0x08,    3 /* Private */,
+       5,    0,  119,    2, 0x08,    4 /* Private */,
+       6,    0,  120,    2, 0x08,    5 /* Private */,
+       7,    0,  121,    2, 0x08,    6 /* Private */,
+       8,    0,  122,    2, 0x08,    7 /* Private */,
+       9,    0,  123,    2, 0x08,    8 /* Private */,
+      10,    0,  124,    2, 0x08,    9 /* Private */,
+      11,    0,  125,    2, 0x08,   10 /* Private */,
+      12,    0,  126,    2, 0x08,   11 /* Private */,
+      13,    0,  127,    2, 0x08,   12 /* Private */,
+      14,    0,  128,    2, 0x08,   13 /* Private */,
+      15,    0,  129,    2, 0x08,   14 /* Private */,
+      16,    0,  130,    2, 0x08,   15 /* Private */,
+      17,    0,  131,    2, 0x08,   16 /* Private */,
+      18,    0,  132,    2, 0x08,   17 /* Private */,
 
  // slots: parameters
     QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
-    QMetaType::Void, 0x80000000 | 7,    8,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
@@ -139,9 +145,12 @@ void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, 
         case 12: _t->on_actionCzcionka_triggered(); break;
         case 13: _t->on_actionKontakt_triggered(); break;
         case 14: _t->on_actionWyszukajWInternecie_triggered(); break;
+        case 15: _t->on_actionZawijanie_wierszy_triggered(); break;
+        case 16: _t->on_actionDzisiejszaData_triggered(); break;
         default: ;
         }
     }
+    (void)_a;
 }
 
 const QMetaObject MainWindow::staticMetaObject = { {
@@ -152,7 +161,7 @@ const QMetaObject MainWindow::staticMetaObject = { {
     nullptr,
 qt_incomplete_metaTypeArray<qt_meta_stringdata_MainWindow_t
 , QtPrivate::TypeAndForceComplete<MainWindow, std::true_type>
-, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<QCloseEvent *, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>
+, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>
 
 
 >,
@@ -179,13 +188,13 @@ int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 15)
+        if (_id < 17)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 15;
+        _id -= 17;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 15)
+        if (_id < 17)
             *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType();
-        _id -= 15;
+        _id -= 17;
     }
     return _id;
 }
